366312350432a0b6d06f407c70704c5313723374
noprom <tyee.noprom@qq.com>
2016-04-11 10:04:55 +0800

    change alpha

--- ch07/cube.asm
+++ ch07/cube.asm
@@ -1,23 +1,23 @@
 ﻿STACKSG	      SEGMENT  STACK  'STK'
 	DW 32 DUP('S')
 STACKSG      ENDS
-	
+
 DATA    	SEGMENT
 TABLE	LABEL    WORD
 	X=5
 	REPT 6	                     ;重复6次
 	     DW X*X*X	   ;定义立方值
 	     X=X+1
-	ENDM	
+	ENDM
 DATA    	ENDS
 
 CODE   	SEGMENT
 MAIN    	PROC FAR
         	ASSUME CS:CODE,DS:DATA,SS:STACKSG
-        	
+
         	MOV	AX,DATA
         	MOV	DS,AX
-        	
+
         	MOV	AX,4C00H
         	INT	21H
 
